3accfbf2cf697c72fa21e6ebc3e39279b68bd3e7
Giorgos <giorgos@giorgos.nl>
2021-02-25 21:53:56 +0100

    list of meows

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,19 +18,44 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import dev.chrisbanes.accompanist.coil.CoilImage
+
+data class Kitten(val index: Int) {
+
+    val imageUrl = "$baseUrl${300 + index}/300"
+
+    companion object {
+        const val baseUrl = "http://placekitten.com/"
+    }
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val kittens = (0..100).map { Kitten(it) }
+
         setContent {
             MyTheme {
-                MyApp()
+                MyApp(kittens)
             }
         }
     }
@@ -38,9 +63,38 @@ class MainActivity : AppCompatActivity() {
 
 // Start building your app here!
 @Composable
-fun MyApp() {
+fun MyApp(kittens: List<Kitten>) {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        LazyColumn(modifier = Modifier.fillMaxHeight()) {
+            items(items = kittens, itemContent = { kitten ->
+
+                Card(
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier
+                        .wrapContentHeight()
+                        .fillMaxWidth()
+                        .height(150.dp)
+                        .padding(16.dp)
+                ) {
+                    Row(Modifier.padding(16.dp)) {
+                        CoilImage(
+                            data = kitten.imageUrl,
+                            contentDescription = null,
+                            modifier = Modifier.size(64.dp).align(Alignment.CenterVertically)
+                        )
+
+                        Spacer(Modifier.width(8.dp))
+
+                        Text(
+                            text = "Meow ${kitten.index}",
+                            style = MaterialTheme.typography.subtitle2,
+                            textAlign = TextAlign.End,
+                            modifier = Modifier.weight(1f).align(Alignment.CenterVertically)
+                        )
+                    }
+                }
+            })
+        }
     }
 }
 
@@ -48,7 +102,7 @@ fun MyApp() {
 @Composable
 fun LightPreview() {
     MyTheme {
-        MyApp()
+        MyApp(emptyList())
     }
 }
 
@@ -56,6 +110,6 @@ fun LightPreview() {
 @Composable
 fun DarkPreview() {
     MyTheme(darkTheme = true) {
-        MyApp()
+        MyApp(emptyList())
     }
 }
